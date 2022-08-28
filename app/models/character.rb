d5568ac60a6cc166f9d532ab48dc958182c3d1f7
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-28 22:14:39 +0300

    commit tree bf0c590e0c2617b2695dad05e933580381044642

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,10 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to(:show) #creates a show instance method that helps know which show instance is the owner of the character    
+  belongs_to(:actor)
+
+
+  def say_that_thing_you_say #returns a string with the character's name and catchphrase
+    "#{self.name} always says: #{self.catchphrase}"
+  end
+    
 end
